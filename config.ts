4213eedb1d1705c3d85fa3e8026ae7d5f76a50db
Moorthy <psoms90@gmail.com>
2020-03-15 19:18:14 +0530

    first commit

---
+++ config.ts
@@ -0,0 +1,38 @@
+
+
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+let browser=require('protractor/built');
+
+exports.config = {
+    // launch locally when fields directConnect and seleniumAddress are not provided
+    geckoDriver: './webdriver-manager/geckodriver-v0.26.0',
+    seleniumServerJar: './webdriver-manager/selenium-server-standalone-3.141.59.jar',
+    specs: ['./ConvertedJSFiles/Spec/test.js'],
+    capabilities: {
+      browserName: 'firefox'
+    },
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 90000
+      },
+
+      onPrepare: function(){
+        /**Jasmine spec reporter */
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailuredSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }));
+
+    /**Allure reporter */
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+    /*After complete the test use-- allure serve 'path of xml' --to get report on browser*/
+
+   },
+   
+  
+  }
+  
